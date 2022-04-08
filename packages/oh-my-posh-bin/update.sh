96c9a4b1fa0f55aa89dbe67bf69b25f846df2860
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-04-08 17:55:10 +0200

    feat: 🎸 Update update script

--- packages/oh-my-posh-bin/update.sh
+++ packages/oh-my-posh-bin/update.sh
@@ -2,21 +2,20 @@
 cd "$(dirname "$0")"
 source PKGBUILD
 
-echo "Package url: $url"
-echo "Current package version: $pkgver"
-echo "Current package source: $source"
-ver=$(pkgver)
-source=$(echo $source | sed -e "s/$pkgver/$ver/")
-pkgver=$ver
-echo "Updated version: $pkgver"
-echo "Updated source: $source"
-
 sha256sums() {
-    curl -sL "$source" | sha256sum | head -c 64
+    curl -sL "$1" | sha256sum | head -c 64
 }
 
-file=$(cat PKGBUILD)
-sha256="\(\'$(sha256sums)\'\)"
+newver=$(pkgver)
 
-echo "SHA256 Sums: $sha256"
-echo "$file" | sed -e "s/sha256sums=('.*')/sha256sums=$sha256/" | sed -e "s/pkgver=.*/pkgver=$pkgver/" >PKGBUILD
+if [[ "$newver" != "$pkgver" ]]; then
+    echo "Updating package..."
+    source=$(echo $source | sed -e "s/$pkgver/$ver/")
+    file=$(cat PKGBUILD)
+    sha256="('$(sha256sums $source)')"
+    echo "Package version: $newver"
+    echo "SHA256 Sums: $sha256"
+    echo "$file" | sed -e "s/sha256sums=('.*')/sha256sums=$sha256/" | sed -e "s/pkgver=.*/pkgver=$newver/" >PKGBUILD
+else
+    echo "No updates available!"
+fi
