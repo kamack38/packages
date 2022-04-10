6f9af0b2afd64be85c96c70d0b1462fff1e468fe
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-04-10 16:35:12 +0200

    fix: 🐛 Fix `leagueoflegendseune` checksums

--- packages/leagueoflegendseune/tools/chocolateyinstall.ps1
+++ packages/leagueoflegendseune/tools/chocolateyinstall.ps1
@@ -1,15 +1,15 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName     = $env:chocolateyPackageName
-$toolsDir        = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageName = $env:chocolateyPackageName
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName    = $packageName
-  fileType       = 'exe'
-  url            = 'https://lol.secure.dyn.riotcdn.net/channels/public/x/installer/current/live.eune.exe'
-  checksum       = '7cc6ebb8b0db977028b84d31c7dd216b200c5b273c37f43bf3e66dc486d246f2'
-  checksumType   = 'sha256'
-  silentArgs     = '--skip-to-install'
+  packageName  = $packageName
+  fileType     = 'exe'
+  url          = 'https://lol.secure.dyn.riotcdn.net/channels/public/x/installer/current/live.eune.exe'
+  checksum     = 'cb4c9e39845a039bbaf16d6414f08735352b75bf0d17df6d1725a9f252d171f6'
+  checksumType = 'sha256'
+  silentArgs   = '--skip-to-install'
 }
 
 Install-ChocolateyPackage @packageArgs
