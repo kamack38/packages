208a6bba7f88707b2634ceb411b149bfea5a14fb
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-04-19 17:36:26 +0200

    fix: 🐛 Fix valorant checksum

--- packages/valorant/tools/chocolateyinstall.ps1
+++ packages/valorant/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   packageName  = $packageName
   fileType     = 'exe'
   url          = 'https://valorant.secure.dyn.riotcdn.net/channels/public/x/installer/current/live.live.eu.exe'
-  checksum     = '873f7d5ed855da64937257db500c2d5cf65f12004b7233ee4e7a38eb745bbb99'
+  checksum     = 'd4d25c22784df0112da38abe0efc83ec545c09fc990cfb9a1cbc6cfdb9e5c33c'
   checksumType = 'sha256'
   silentArgs   = '--skip-to-install'
 }
