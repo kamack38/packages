0ced01a87530c88b0625c84bc821c857628fc51e
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-06-25 14:27:30 +0200

    feat(packages): 🎸 Update packages & fix portmaster

--- packages/blitz.gg/tools/chocolateyinstall.ps1
+++ packages/blitz.gg/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'exe'
-  url            = 'https://blitz-stable.blitz.gg/Blitz-1.16.77.exe'
-  checksum       = '6de82fb51e8931edcba13159effe1a97570ae2bf9270d04ce56ec49cce451e51'
+  url            = 'https://blitz-stable.blitz.gg/Blitz-1.17.4.exe'
+  checksum       = '543b52c1a28ff74f9640dc62ef38c3a130ea84c0b8726f1ccc8a5a9c547fd35c'
   checksumType   = 'sha256'
   silentArgs     = '/S'
 }
--- packages/portmaster/tools/chocolateyinstall.ps1
+++ packages/portmaster/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $packageName = $env:chocolateyPackageName
 $packageArgs = @{
   packageName  = $packageName
   fileType     = 'exe'
-  url          = 'https://updates.safing.io/windows_amd64/packages/portmaster-installer_v0-7-0.exe'
-  checksum     = '2a7ea1568f83f5579c9921975a05aaf7e853bc29e5f5009d86ee8c1c27c5d336'
+  url          = 'https://updates.safing.io/windows_amd64/packages/portmaster-installer_v0-8-8.exe'
+  checksum     = 'b0c03a122b27165af162a071e51d3997059083715cdb056df62b68acb88280b4'
   checksumType = 'sha256'
   silentArgs   = '/S'
 }
