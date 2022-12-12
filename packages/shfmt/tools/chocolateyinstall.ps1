ba46c4cd307d19c17e494dc2510f3b959bbf35f4
kamack38 <kamack38@users.noreply.github.com>
2022-12-12 18:46:50 +0000

    feat: 🔼 Update chocolatey packages

--- packages/shfmt/tools/chocolateyinstall.ps1
+++ packages/shfmt/tools/chocolateyinstall.ps1
@@ -7,10 +7,10 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName    = $packageName
-  url            = 'https://github.com/mvdan/sh/releases/download/v3.5.1/shfmt_v3.5.1_windows_386.exe'
-  url64bit       = 'https://github.com/mvdan/sh/releases/download/v3.5.1/shfmt_v3.5.1_windows_amd64.exe'
-  checksum       = '50df493c1ac1d849bb0acaf7c88b4a3cc79b0e13bce801c917a305599245c345'
-  checksum64     = 'f759493bc55a4ea181442eb7d79b527ff6c15d62ec567b8b86a5df6a086c9c46'
+  url            = 'https://github.com/mvdan/sh/releases/download/v3.6.0/shfmt_v3.6.0_windows_386.exe'
+  url64bit       = 'https://github.com/mvdan/sh/releases/download/v3.6.0/shfmt_v3.6.0_windows_amd64.exe'
+  checksum       = '3753abfaca1beaf80a81e084c7947e60e2a08076270950d76197f4997fc122d0'
+  checksum64     = '18122d910ba434be366588f37c302c309cde4ca5403f93285254a3cf96839d01'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   fileFullPath   = (Join-Path $toolsDir 'shfmt.exe')
