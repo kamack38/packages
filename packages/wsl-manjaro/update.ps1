748ab1e0184fcbf6198056040ca824d68abd2006
kamack38 <64226248+kamack38@users.noreply.github.com>
2023-02-20 13:35:53 +0100

    refactor(choco: wsl-manjaro): 💡 Get the latest release

--- packages/wsl-manjaro/update.ps1
+++ packages/wsl-manjaro/update.ps1
@@ -12,13 +12,13 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $releases = "https://api.github.com/repos/$repo/releases"
+    $latest_release = "https://api.github.com/repos/$repo/releases/latest"
 
-    $tag = (Invoke-WebRequest -Uri $releases -UseBasicParsing | ConvertFrom-Json)[0].tag_name
-    
+    $tag = (Invoke-WebRequest -Uri $latest_release -UseBasicParsing | ConvertFrom-Json).tag_name    
+
     $version = "$($tag.Substring(0, 4)).$($tag.Substring(4,2)).$($tag.Substring(6,2))"
 
-    $file64 = "ManjaroWSL.zip"
+    $file64 = "ManjaroWSL2.zip"
     $url64 = "https://github.com/$repo/releases/download/$tag/$file64"
 
     return @{
