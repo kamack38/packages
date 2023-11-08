82c1f36c068c1bfa56c298e9314f4c59ee1b203d
kamack38 <kamack38@users.noreply.github.com>
2023-11-08 18:44:06 +0000

    feat: 🔼 Update chocolatey packages

--- packages/windhawk/tools/chocolateyinstall.ps1
+++ packages/windhawk/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $packageName = $env:chocolateyPackageName
 $packageArgs = @{
   packageName  = $packageName
   fileType     = 'exe'
-  url          = 'https://github.com/ramensoftware/windhawk/releases/download/v1.3.1/windhawk_setup.exe'
-  checksum     = 'fb1ecc8b6eeebda10fa06c1c5dce004e53ff17288929c0e77a0daaa384d2edf4'
+  url          = 'https://github.com/ramensoftware/windhawk/releases/download/v1.4/windhawk_setup.exe'
+  checksum     = 'aa5417fb02abf9ae9f470caead6e832c3633b5afef83e38ee9bd85d49277282e'
   checksumType = 'sha256'
   silentArgs   = '/S'
 }
