675f56b0020c101c1eb6757624879f2914e36917
kamack38 <kamack38@users.noreply.github.com>
2024-03-17 18:46:43 +0000

    feat: 🔼 Update chocolatey packages

--- packages/onefetch/tools/chocolateyinstall.ps1
+++ packages/onefetch/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $packageName
   fileType      = 'exe'
-  url           = 'https://github.com/o2sh/onefetch/releases/download/2.19.0/onefetch-win.tar.gz'
-  checksum      = ''
+  url           = 'https://github.com/o2sh/onefetch/releases/download/2.20.0/onefetch-win.tar.gz'
+  checksum      = 'bb78f7e607ab00f3e5550e998ea5d4f376889c402d943ef5eb8b075c2f30a0bb'
   checksumType  = 'sha256'
   unzipLocation = $toolsDir
 }
