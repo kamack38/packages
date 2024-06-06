c54f8c1f293b670d332fab18e0d5e79d48d901f3
kamack38 <kamack38@users.noreply.github.com>
2024-06-06 18:49:00 +0000

    feat: 🔼 Update chocolatey packages
    Former-commit-id: 834eed764e3e493aa07365adea68c5684b2220ad

--- packages/blitz.gg/tools/chocolateyinstall.ps1
+++ packages/blitz.gg/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'exe'
-  url            = 'https://blitz-main.blitz.gg/Blitz-2.1.175.exe'
-  checksum       = '54821893266cfabebf079082d8da0801309489a7424ec9e32c8d3165d50df504'
+  url            = 'https://blitz-main.blitz.gg/Blitz-2.1.176.exe'
+  checksum       = '9654d15d25ee7275e061bb0f3cff1407b9a9682f6961354e46ac680294ea5960'
   checksumType   = 'sha256'
   silentArgs     = '/S'
 }
