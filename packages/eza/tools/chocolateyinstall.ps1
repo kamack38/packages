b88609fbfd442a7fbcff846cd2fd77a68d7d9be1
kamack38 <64226248+kamack38@users.noreply.github.com>
2025-06-09 10:51:53 +0200

    fix(eza): 🐛 Don't duplicate variables

--- packages/eza/tools/chocolateyinstall.ps1
+++ packages/eza/tools/chocolateyinstall.ps1
@@ -6,13 +6,9 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'exe'
-  fileType64     = 'exe'
   url            = 'https://github.com/eza-community/eza/releases/download/v0.21.4/eza.exe_x86_64-pc-windows-gnu.zip'
-  url64bit       = 'https://github.com/eza-community/eza/releases/download/v0.21.4/eza.exe_x86_64-pc-windows-gnu.zip'
   checksum       = '0d262a4565f99a32c19a77a731407955e46b524cf51a1662b1854635a02553db'
-  checksum64     = '0d262a4565f99a32c19a77a731407955e46b524cf51a1662b1854635a02553db'
   checksumType   = 'sha256'
-  checksumType64 = 'sha256'
   unzipLocation  = $toolsDir
 }
 
--- packages/eza/update.ps1
+++ packages/eza/update.ps1
@@ -6,9 +6,7 @@ function global:au_SearchReplace {
     @{
         ".\tools\chocolateyInstall.ps1" = @{
             "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
             "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
     }
 }
@@ -18,15 +16,11 @@ function global:au_GetLatest {
 
     $tag = (Invoke-WebRequest -Uri $releases -UseBasicParsing | ConvertFrom-Json)[0].tag_name
 
-    $file64 = "eza.exe_x86_64-pc-windows-gnu.zip"
-    $url64 = "https://github.com/$repo/releases/download/$tag/$file64"
-
     $file32 = "eza.exe_x86_64-pc-windows-gnu.zip"
     $url32 = "https://github.com/$repo/releases/download/$tag/$file32"
 
     return @{
         URL32   = $url32
-        URL64   = $url64
         Version = $tag.Replace('v', '')
     }
 }
